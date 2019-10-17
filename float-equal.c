1b89b93a0db8a8a48bee0956a97e705cf82daa47
Sanozhi <1459880131@qq.com>
2019-10-18 00:15:39 +0800

    helloworld add commit

--- float-equal.c
+++ float-equal.c
@@ -5,29 +5,28 @@
 #include <stdio.h>
 #include <math.h>
 
-
 int main()
 
 {
-    float m,x,c;
+
+    float m = 1.78,a;
+
+    float x = 1.779999, y = 2;
+
+    float z = m / y, z1 = x / y;
     
-    printf("Enter a number m:");
-    scanf("%f",&m);
-    printf("Enter another number x:");
-    scanf("%f",&x);
-    
-    c=x-m;
-    c=fabs(c);
-    
-    if (c<0.00001)
+	if (fabs(z-z1)<1e-6) 
     {
-    	printf("result: m=x\n");
-	}
-	else
-	{
-		printf("result: m!=x\n");
-	}
-    
-	return 0;
+	
+
+        z = (m + x) / y;
+
+    }
+
+    printf("Z is %f\n", z);
+
+    printf("Z1 is %f\n", z1);
+
+    return 0;
 
 }
